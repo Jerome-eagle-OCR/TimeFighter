3098a19213db0a688577545d39984324fcd0cc08
Jerome Rigault <jerome.rigault@expleogroup.com>
2021-07-17 19:11:02 +0200

    Game done Next step: debugging, lesson 30

--- app/src/main/java/com/raywenderlich/timefighter/MainActivity.kt
+++ app/src/main/java/com/raywenderlich/timefighter/MainActivity.kt
@@ -1,11 +1,94 @@
 package com.raywenderlich.timefighter
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+import com.raywenderlich.timefighter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var score = 0
+
+    private lateinit var tapMeButton: Button
+    private lateinit var gameScoreTextView: TextView
+    private lateinit var timeLeftTextView: TextView
+
+    private var gameStarted: Boolean = false
+
+    private lateinit var countDownTimer: CountDownTimer
+    private val initialCountDown: Long = 60000
+    private val countDownInterval: Long = 1000
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        initActivity()
+
+        resetGame()
+    }
+
+    private fun initActivity() {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        tapMeButton = binding.tapMeButton
+        gameScoreTextView = binding.gameScoreTextView
+        timeLeftTextView = binding.timeLeftTextView
+
+        tapMeButton.setOnClickListener { incrementScore() }
+    }
+
+    private fun resetGame() {
+        score = 0
+
+        gameScoreTextView.text = getString(R.string.yourScore, score)
+
+        val initialTimeLeft = initialCountDown / 1000
+        timeLeftTextView.text = getString(R.string.timeLeft, initialTimeLeft)
+
+        countDownTimer = object : CountDownTimer(initialCountDown, countDownInterval) {
+            override fun onTick(millisUntilFinished: Long) {
+                val timeLeft = millisUntilFinished / 1000
+                timeLeftTextView.text = getString(R.string.timeLeft, timeLeft)
+            }
+
+            override fun onFinish() {
+                endGame()
+            }
+        }
+
+        gameStarted = false
+    }
+
+    private fun incrementScore() {
+        if (!gameStarted) {
+            startGame()
+        }
+
+        score++
+        val newScore = getString(R.string.yourScore, score)
+        gameScoreTextView.text = newScore
+    }
+
+    private fun startGame() {
+        countDownTimer.start()
+        gameStarted = true
+    }
+
+    private fun endGame() {
+        tapMeButton.isClickable = false
+        Snackbar.make(
+            binding.root,
+            getString(R.string.gameOverMessage, score),
+            Snackbar.LENGTH_INDEFINITE,
+        ).setAction("OK") {
+            tapMeButton.isClickable = true
+            resetGame()
+        }.show()
     }
 }
